0c08e2c45a107ce03d9a757d10f0dd0e1a808081
Leo-0 <1043970927@qq.com>
2017-08-08 04:30:25 +0800

    优化与改动
    优化获取最低高度的索引的算法，删除冗余代码，新增一个默认参数，用于对所有的pSelector进行布局

--- assets/js/wff.js
+++ assets/js/wff.js
@@ -6,85 +6,85 @@
 		}
 	}
 
-	function orderedPos(arr) { // 获取排序后每个值的原来位置
-		var len = arr.length;
-		var a = [];
-		var t = [];
-		for (var m = 0; m < len; m++) {
-			a[m] = m;
-			t[m] = arr[m];
-		}
-		var temp;
-		for (var i = 0; i < len; i++) {
-			var flag = 0;
-			for (var j = 0; j < len - 1 - i; j++) {
-				if (t[j] > t[j + 1]) {
-					flag = 1;
-					temp = t[j];
-					t[j] = t[j + 1];
-					t[j + 1] = temp;
-					temp = a[j];
-					a[j] = a[j + 1];
-					a[j + 1] = temp;
-				}
-			}
-			if (flag === 0) {
-				break;
+	function getMinHeightPos(arr) { // 获取最低高度的索引
+		var min = Math.min.apply(null, arr);
+		for (var key in arr) {
+			if (arr[key] === min) {
+				return key;
 			}
 		}
-		return a;
 	}
 	Wff.prototype = {
 		init: function(option) {
 			var _default = {
 				pSelector: '',
-				itemSelector: ''
+				itemSelector: '',
+				all: false // all for all the pSelector
 			};
 			var opt = option;
 			this.pSelector = opt.pSelector || _default.pSelector;
 			this.itemSelector = opt.itemSelector || _default.itemSelector;
-			this.container = document.querySelector(this.pSelector);
-			if (this.container) {
-				this.container.style.position = 'relative';
-				this.setPos(this.container, this.itemSelector);
-				var that = this;
-				this.addEvent(window, 'resize', function() {
-					that.setPos(that.container, that.itemSelector);
-				});
-				this.addEvent(window, 'load', function() {
-					that.setPos(that.container, that.itemSelector);
+			if (opt.all) {
+				this.all = opt.all;
+			} else {
+				this.all = _default.all;
+			}
+			var that = this;
+			if (!this.all) {
+				this.container = document.querySelector(this.pSelector);
+				if (this.container) {
+					this.container.style.position = 'relative';
+					this.setPos(this.container, this.itemSelector);
+					this.addEvent(window, 'resize', function() {
+						that.setPos(that.container, that.itemSelector);
+					});
+					this.addEvent(window, 'load', function() {
+						that.setPos(that.container, that.itemSelector);
+					});
+				}
+			} else {
+				this.container = document.querySelectorAll(this.pSelector);
+				this.container.forEach(function(con) {
+					con.style.position = 'relative';
+					that.setPos(con, that.itemSelector);
+					that.addEvent(window, 'resize', function() {
+						that.setPos(con, that.itemSelector);
+					});
+					that.addEvent(window, 'load', function() {
+						that.setPos(con, that.itemSelector);
+					});
 				});
 			}
 		},
 		setPos: function(ele, selector) {
 			var sel = ele.querySelector(selector);
+			if (!sel) {
+				return false;
+			}
 			var sels = ele.querySelectorAll(selector);
 			var pw = ele.offsetWidth;
 			var w = sel.offsetWidth;
 			var col = Math.round(pw / w);
 			var pos_arr = new Array(col);
-			var a = orderedPos(pos_arr);
+			var a = getMinHeightPos(pos_arr);
 			sels.forEach(function(s, i) {
 				var h = s.offsetHeight;
 				s.style.position = 'absolute';
-				if (Math.floor(i / col) === 0) {
+				if (Math.floor(i / col) === 0) { // 第一行
 					s.style.left = i % col * w + 'px';
 					s.style.top = 0 + 'px';
-				} else {
-					s.style.left = a[0] * w + 'px';
-					s.style.top = pos_arr[a[0]] + 'px';
-				}
-				if (Math.floor(i / col) === 0) { // 第一行
 					pos_arr[i % col] = h + s.offsetTop;
-				} else {
-					pos_arr[a[0]] = h + s.offsetTop; // 其他行总是从距容器最短的位置插入
+				} else { // 其他行总是从距容器最短的位置插入
+					s.style.left = a * w + 'px';
+					s.style.top = pos_arr[a] + 'px';
+					pos_arr[a] = h + s.offsetTop;
 				}
-				a = orderedPos(pos_arr); // 每插入一次重新获取最小位置
+				a = getMinHeightPos(pos_arr); // 每插入一次重新获取最小位置
 			});
 			var ha = pos_arr.sort(function(a, b) {
 				return b - a;
 			});
-			this.container.style.height = ha[0] + 'px';
+			ele.style.height = ha[0] + 'px';
 		},
 		addEvent: function(obj, evt, fn) {
 			if (document.addEventListener) {
